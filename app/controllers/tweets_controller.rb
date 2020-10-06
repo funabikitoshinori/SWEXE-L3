04ff3ce4286f165bb4f39d766e37e95292f9ba69
funabiki <1811140053m@ed.fuk.kindai.ac.jp>
2020-10-06 06:37:48 +0000

    A.txtを作成

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,34 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all 
+  end
+  def new
+    @tweets = Tweet.new
+  end
+  def create
+    @tweets = Tweet.new(message: params[:tweet][:message] ,tdate: Time.current[:tweet][:tdate])
+    if @tweets.save
+      flash[:notice] = '1レコード追加しました'
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    tweet = Tweet.find(params[:id])
+      tweet.destroy
+      redirect_to'/'
+  end
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(message: params[:tweet][:message])
+    redirect_to
+  end
+  
+end
